19af12611e07d7987d4c63baeb8f834c36bb2922
Motts182 <mnd.matias.diaz@gmail.com>
2016-07-19 10:01:39 -0300

    First commit have a base code, jump, cfg camera and etc..

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+     
+    Rigidbody2D rb2d;
+    public bool ground;
+    
+    void Awake() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+
+        if (Input.GetButtonDown("Fire1") && ground)
+        {
+            rb2d.AddForce(Vector2.up * 5 , ForceMode2D.Impulse);
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //print("Enter");
+        ground = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //print("Exit");
+        ground = false;
+    }
+}
